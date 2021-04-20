3f8fa5654d7921a1a79f305c25ed46df3ba9d6ca
Jose Antonio <jose@king.bwrouter>
2021-04-19 20:09:54 -0500

    Anado estilos

--- src/config/server.js
+++ src/config/server.js
@@ -8,7 +8,8 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'../app/views/'));
-
+// Cargamos archivos estaticos
+app.use(express.static('public'));
 
 // middleware
 app.use(bodyParser.urlencoded({extended: false}));
